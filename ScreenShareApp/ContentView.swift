81591f5115931ac389d2b18bc4c29b318fef74e7
Rajesab Nazirahammad Yadahalli <rajesab.ny@wisdomleaf.com>
2024-08-07 21:16:09 +0530

    Initial Commit

---
+++ ScreenShareApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenShareApp
+//
+//  Created by Rajesab N Y on 07/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenShareApp/ScreenShareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShareAppApp.swift
+//  ScreenShareApp
+//
+//  Created by Rajesab N Y on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
