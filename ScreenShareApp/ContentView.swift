7b55266da09318595bc0e70ed13b9488f7a73c15
Rajesab Nazirahammad Yadahalli <127076082+Rajesab-N-Y@users.noreply.github.com>
2024-08-07 21:55:49 +0530

    UI Implementation

--- ScreenShareApp/ContentView.swift
+++ ScreenShareApp/ContentView.swift
@@ -9,17 +9,38 @@ import SwiftUI
 import HMSSDK
 
 struct ContentView: View {
+    @State var roomCode: String? = nil
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            if let roomCode = roomCode {
+                Text("Room Code: \(roomCode)")
+                    .padding()
+                
+                Button(action: joinRoom) {
+                    Text("Join Room")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+                
+            }
+        } //: VStack
+        
+    } //: body
+    
+    // Join Room Logic
+    func joinRoom() {
+        // Implement the join room logic here
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(roomCode: "1234")
+    }
+
 }
+
